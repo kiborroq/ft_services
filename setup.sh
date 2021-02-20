dfdc642e6252be154f2448b3e8dd3aee34a476c4
kiborroq <kiborroq@kiborroq.42.fr>
2021-02-20 12:35:39 +0300

    finish

--- setup.sh
+++ setup.sh
@@ -1,54 +1,54 @@
 #!/bin/sh
 
 # Delete and then start Minikube
-echo "Delete and then start Minikube"
+echo "\033[1;32mDelete and then start Minikube\033[0m"
 minikube delete
 minikube start --driver=virtualbox
 
 # Start Kubernetes dashboard
-echo "Start Kubernetes dashboard"
+echo "\033[1;32mStart Kubernetes dashboard\033[0m"
 minikube dashboard &
 
 # Adjust MetalLB
-echo "Adjust MetalLB"
+echo "\033[1;32mSetup MetalLB\033[0m"
 minikube addons enable metallb
 kubectl apply -f srcs/yamls/metallb.yaml
 
 # Init Docker in Minikube
-echo "Init Docker in Minikube"
+echo "\033[1;32mSetup Docker in Minikube\033[0m"
 eval $(minikube docker-env)
 
 # Setup an infrastructure of services
-echo "Setup an infrastructure of services"
+echo "\033[1;32mSetup an infrastructure of services\033[0m"
 
-echo "Setup NGINX"
-docker build -t nginx-img      srcs/images/nginx/
-kubectl apply -f               srcs/yamls/nginx.yaml
-
-echo "Setup PHPMYADMIN"
+echo "\033[1;32mSetup PHPMYADMIN\033[0m"
 docker build -t phpmyadmin-img srcs/images/phpmyadm/
 kubectl apply -f               srcs/yamls/phpmyadmin.yaml
 
-echo "Setup WRODPRESS"
+echo "\033[1;32mSetup WRODPRESS\033[0m"
 docker build -t wordpress-img  srcs/images/wordpress/
 kubectl apply -f               srcs/yamls/wordpress.yaml
 
-echo "Setup MYSQL"
+echo "\033[1;32mSetup MYSQL\033[0m"
 docker build -t mysql-img      srcs/images/mysql/
 kubectl apply -f               srcs/yamls/mysql.yaml
 
-echo "Setup INFLUXDB"
+echo "\033[1;32mSetup NGINX\033[0m"
+docker build -t nginx-img      srcs/images/nginx/
+kubectl apply -f               srcs/yamls/nginx.yaml
+
+echo "\033[1;32mSetup INFLUXDB\033[0m"
 docker build -t influxdb-img   srcs/images/influxdb/
 kubectl apply -f               srcs/yamls/influxdb.yaml
 
-echo "Setup GRAFANA"
+echo "\033[1;32mSetup GRAFANA\033[0m"
 docker build -t grafana-img    srcs/images/grafana/
 kubectl apply -f               srcs/yamls/grafana.yaml
 
-echo "Setup TELEGRAF"
+echo "\033[1;32mSetup TELEGRAF\033[0m"
 docker build -t telegraf-img   srcs/images/telegraf/
 kubectl apply -f               srcs/yamls/telegraf.yaml
 
-echo "Setup FTPS"
+echo "\033[1;32mSetup FTPS\033[0m"
 docker build -t ftps-img       srcs/images/ftps/
 kubectl apply -f               srcs/yamls/ftps.yaml
--- srcs/images/mysql/run_container.sh
+++ srcs/images/mysql/run_container.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
-# rc-status -a
-# touch /run/openrc/softlevel
-
-# mysql_install_db -u root --datadir=/var/lib/mysql
-# /usr/bin/mysqld -u root --datadir=/var/lib/mysql --pid-file=/run/mysqld/mysqld.pid & sleep 3
-
-# rc-service mariadb start
-
-# mysql -u root -e "CREATE DATABASE wordpress; CREATE USER 'kiborroq'@'%' IDENTIFIED BY 'kiborroq'; GRANT ALL PRIVILEGES ON wordpress . * TO 'kiborroq'@'%'; FLUSH PRIVILEGES;"
-# mysql -u root  wordpress < wordpress.sql
 
 mysql_install_db -u root --datadir=/var/lib/mysql
-/usr/bin/mysqld -u root & sleep 3
+/usr/bin/mysqld -u root --pid-file=/run/mysqld/mysqld.pid & sleep 3
 
 mysql -u root -e "CREATE DATABASE wordpress;CREATE USER 'kiborroq'@'%' IDENTIFIED BY 'kiborroq';GRANT ALL PRIVILEGES ON * . * TO 'kiborroq'@'%';FLUSH PRIVILEGES;"
-mysql -uroot  wordpress < wordpress.sql
+mysql -u root wordpress < wordpress.sql
+
+kill $(cat /run/mysqld/mysqld.pid) & sleep 3
 
 supervisord -c /etc/supervisord.conf
-
-sleep infinity
